fd774c62062d427e62bf703a17f5d5a4eaf28041
Vera <veray@sfu.ca>
2019-09-09 16:08:10 -0700

    Create Gift.java

---
+++ Santa on Christmas Eve/src/house/Gift.java
@@ -0,0 +1,31 @@
+package house;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+public class Gift extends Decoration{
+	
+	public int giftX1, giftY1, giftW, giftH;
+	public int giftX2, giftY2;
+	public int giftX3, giftY3;
+	public int giftX4, giftY4;
+	
+	public Rectangle2D.Double gitftBody1, gitftBody2, gitftBody3, gitftBody4;
+	
+	
+	public Gift(){
+		
+		
+		
+	}
+	
+	
+	@Override
+	public void display(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
