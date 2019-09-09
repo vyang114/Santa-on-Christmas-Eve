60a89b14e0e77d70f51284b4d96d522059c98f95
Vera <veray@sfu.ca>
2019-09-09 16:08:38 -0700

    Create ImageLoader.java

---
+++ Santa on Christmas Eve/src/others/ImageLoader.java
@@ -0,0 +1,31 @@
+package others;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+	
+	//method to load images
+	public static BufferedImage loadImage(String imgFile) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(imgFile));
+		}catch (IOException e) {
+		}
+	return img;
+	}
+	
+	public static boolean saveImage(BufferedImage img, String fileName, String fileFormat) {
+		try {
+
+			File saveFile = new File(fileName + "." + fileFormat);
+			ImageIO.write(img, fileFormat, saveFile);
+
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+}
